6e063a592d5f474fbb6c5a4a81d724e0011eba25
Jack McBride <eternalfalter@yahoo.com>
2016-06-30 16:42:10 -0600

    Pantry Update

--- public/js/pantry.js
+++ public/js/pantry.js
@@ -2,29 +2,27 @@ var app = angular.module("mealPlannerApp");
 
 app.controller("PantryController", ["$scope", "theService", function ($scope, theService) {
 
-   var pantry = [
-        {
-            title: "Eggs",
-            quantity: 12,
-            expiration: 3,
-            url: "http://www.browneggsandlamb.com/images/stories/4eggs.png"
-        },
-        {
-            title: "Cilantro Bunch",
-            quantity: 2,
-            expiration: 5,
-            url: "http://bengardranch.com/wp-content/uploads/2012/03/cilantro_i.png"
-        }
-    ]
-    
-    
-     $scope.theService.getPantry.then(function (response) {
+    $scope.theService = theService;
+
+    //   var pantry = [
+    //        {
+    //            title: "Eggs",
+    //            quantity: 12,
+    //            expiration: 3,
+    //            url: "http://www.browneggsandlamb.com/images/stories/4eggs.png"
+    //        },
+    //        {
+    //            title: "Cilantro Bunch",
+    //            quantity: 2,
+    //            expiration: 5,
+    //            url: "http://bengardranch.com/wp-content/uploads/2012/03/cilantro_i.png"
+    //        }
+    //    ]
+
+    $scope.theService.getPantry.then(function (response) {
         $scope.pantry = response.data;
     });
-    
     $scope.theService.deleteIngredient(ingredient).then(function (response) {
         $scope.pantry = response.data;
     });
-    
-    
 }]);
--- public/js/services.js
+++ public/js/services.js
@@ -2,35 +2,37 @@ var app = angular.module("mealPlannerApp");
 
 app.service("theService", ["$http", function ($http) {
 
+    var baseUrl = "http://localhost:8000/api/";
+
     //******RECIPES******
 
     //Get all Recipes
 
     this.getRecipes =
-        $http.get("http://localhost:8000/recipes");
+        $http.get(baseUrl + "recipes");
 
     //Get single Recipe
 
     this.getRecipe = function (recipeKey) {
-        return $http.get("http://localhost:8000/recipes/" + recipeKey);
+        return $http.get(baseUrl + "recipes/" + recipeKey);
     };
 
     //Add new Recipe
 
     this.postRecipe = function (recipe) {
-        return $http.post("http://localhost:8000/recipes/", recipe);
+        return $http.post(baseUrl + "recipes/", recipe);
     };
 
     //Edit Recipe
 
     this.putRecipe = function (key, recipe) {
-        return $http.put("http://localhost:8000/recipes/" + recipe._id, recipe);
+        return $http.put(baseUrl + "recipes/" + recipe._id, recipe);
     };
 
     //Delete Recipe
 
     this.deleteRecipe = function (recipe) {
-        return $http.delete("http://localhost:8000/recipes" + recipe._id);
+        return $http.delete(baseUrl + "recipes" + recipe._id);
     };
 
     //******PANTRY******
@@ -38,24 +40,24 @@ app.service("theService", ["$http", function ($http) {
     //List ingredients in Pantry
 
     this.getPantry =
-        $http.get("http://localhost:8000/pantry");
+        return $http.get(baseUrl + "pantry");
 
     //Add Ingredient to Pantry
 
     this.postIngredient = function (ingredient) {
-        return $http.post("http://localhost:8000/pantry/", ingredient);
+        return $http.post(baseUrl + "pantry/", ingredient);
     };
 
     //Change Ingredient Quantity
 
     this.putIngredient = function (key, ingredient) {
-        return $http.put("http://localhost:8000/pantry/" + ingredient._id, ingredient);
+        return $http.put(baseUrl + "pantry/" + ingredient._id, ingredient);
     };
 
     //Delete Ingredient in Pantry
 
     this.deleteIngredient = function (ingredient) {
-        return $http.delete("http://localhost:8000/pantry" + ingredient._id);
+        return $http.delete(baseUrl + "pantry" + ingredient._id);
     };
 
     //******CALENDAR******
